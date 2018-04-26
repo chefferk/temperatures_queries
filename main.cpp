319a1bdb6969927ce310e9da5290a2e3f97b21be
Keaton Cheffer <chefferk@tamu.edu>
2018-04-26 16:57:55 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include "TemperatureDatabase.h"
-
+#include <vector>
+#include <fstream>
+#include <algorithm>
 #include <iostream>
 using namespace std;
+
+// Description: Reads information from file and stores values in 2D vector
+// Input: Filename, number of rows and columns
+// Output: 2D vector with elevation numbers
+vector<vector<int> > loadImage(int &numRows, int &numColumns, string fileName) {
+    // Start infile stream and load with given filename
+    ifstream inFile (fileName);
+
+    // Check if able to open
+    if (!inFile.is_open()) {
+        cout << "Unable to open " << fileName << endl;
+        exit(1);
+    }
+
+    // Create vector to store data
+    vector<vector<int> > values(numRows, vector<int> (numColumns));
+
+    // Store elevation data in 2d vector
+    int elevation = -1;
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numColumns; ++j) {
+            inFile >> elevation;
+            values.at(i).at(j) = elevation;
+        }
+    }
+
+    // Close file stream
+    inFile.close();
+
+    // Return the pixel values
+    return values;
+}
 
 int main(int argc, char** argv) {
 	if (argc < 3) {
@@ -12,4 +46,6 @@ int main(int argc, char** argv) {
 		database.loadData(argv[1]);
 		database.performQuery(argv[2]);
 	}
+
+
 }
